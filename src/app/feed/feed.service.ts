bf625ac62949ba42f94e57cedded56aa63ed1f74
nhuvu98 <nguyennhuvu98@gmail.com>
2019-11-15 14:32:37 +0700

    add personal posts page

--- src/app/feed/feed.service.ts
+++ src/app/feed/feed.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Article } from '../articles/shared/article.model';
+import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +18,25 @@ export class FeedService {
           console.log(articleList);
           resolve(articleList);
         },
-        error => {
+        failure => {
           resolve(null);
         }
       )
     });
+  }
+
+  getPersonalPosts(username: string): Promise<{data: Article[], message: string}> {
+    return new Promise((resolve, reject) => {
+      this.http.get(`/api/feed/${username}`, { observe: 'response'}).subscribe(
+        success => {
+          let articleList = success.body as Article[];
+          resolve({data: articleList, message: 'Success'});
+        },
+        failure => {
+          resolve({data: null, message: failure.error.message});
+        }
+      )
+    })
   }
 
 }
--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { UserProfile } from '../shared/user-profile.model';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../shared/user.service';
+import { ErrorLogService } from 'src/app/shared/error-log.service';
+import { Article } from 'src/app/articles/shared/article.model';
+import { ArticleService } from 'src/app/articles/shared/article.service';
+import { FeedService } from 'src/app/feed/feed.service';
+import { SessionService } from 'src/app/shared/session.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +15,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: UserProfile;
+  isPersonal = true;
+
+  articles = [];
+
+  constructor(
+    private routes: ActivatedRoute,
+    private userService: UserService,
+    private feedService: FeedService,
+    private errorService: ErrorLogService,
+    private sessionService: SessionService
+  ) { }
 
   ngOnInit() {
+    this.load();
+    this.requestOwnPosts();
+  }
+
+  load() {
+    let username = this.routes.snapshot.params['username'];
+    this.userService.loadProfile(username).then(
+      response => {
+        if (response.data) {
+          this.user = response.data;
+        } else {
+          this.errorService.notify(response.message);
+        }
+      }
+    )
+  }
+
+  requestOwnPosts() {
+    this.isPersonal = true;
+    this.feedService.getPersonalPosts(this.sessionService.getSession().username).then(
+      response => {
+        if (response.data) {
+          this.articles = response.data;
+          console.log(response.data);
+        } else {
+          this.errorService.notify(response.message);
+        }
+      }
+    )
+  }
+
+  requestFavoritePosts() {
+    this.isPersonal = false;
   }
 
 }
